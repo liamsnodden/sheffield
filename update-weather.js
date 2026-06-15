2978369d6c7cad3b728a23a3deaa2b82de102030
liamsnodden <liam.snodden@sherwin.com>
2026-06-15 12:22:42 +0100

    Update update-weather.js

--- update-weather.js
+++ update-weather.js
@@ -50,38 +50,103 @@ const iconMap = {
 
 async function updateWeather() {
 
+
+
 const response = await fetch(
-'https://api.open-meteo.com/v1/forecast?latitude=53.3811&longitude=-1.4701&current=temperature_2m,apparent_temperature,weather_code'
+‘https://api.open-meteo.com/v1/forecast?latitude=53.3811&longitude=-1.4701&current=temperature_2m,apparent_temperature,weather_code&daily=weather_code,temperature_2m_max&forecast_days=4’
 );
+
+
 
 const data = await response.json();
 
+
+
 const code = data.current.weather_code;
-const icon = iconMap[code] || '04d';
+const icon = iconMap[code] || ‘04d’;
 
-let html = fs.readFileSync('jsweather.html', 'utf8');
 
-html = html.replace(
-'%%TEMP%%',
-Math.round(data.current.temperature_2m)
+
+const days = data.daily.time;
+
+
+
+const dayNames = days.map(d =>
+new Date(d).toLocaleDateString(
+‘en-GB’,
+{ weekday:‘short’ }
+).toUpperCase()
 );
 
-html = html.replace(
-'%%FEELS%%',
-Math.round(data.current.apparent_temperature)
-);
 
-html = html.replace(
-'%%CONDITION%%',
-weatherDescriptions[code] || 'Unknown'
-);
 
-html = html.replace(
-'%%ICON%%',
-`${icon}.png`
-);
+const icon1 = iconMap[data.daily.weather_code[1]] || ‘04d’;
+const icon2 = iconMap[data.daily.weather_code[2]] || ‘04d’;
+const icon3 = iconMap[data.daily.weather_code[3]] || ‘04d’;
 
-fs.writeFileSync('jsweatherupdated.html', html);
+
+
+let html = fs.readFileSync(‘jsweather.html’, ‘utf8’);
+
+
+
+html = html.replace(’%%TEMP%%’,
+Math.round(data.current.temperature_2m));
+
+
+
+html = html.replace(’%%FEELS%%’,
+Math.round(data.current.apparent_temperature));
+
+
+
+html = html.replace(’%%CONDITION%%’,
+weatherDescriptions[code] || ‘Unknown’);
+
+
+
+html = html.replace(’%%ICON%%’,
+${icon}.png);
+
+
+
+html = html.replace(’%%DAY1%%’, dayNames[1]);
+html = html.replace(’%%DAY2%%’, dayNames[2]);
+html = html.replace(’%%DAY3%%’, dayNames[3]);
+
+
+
+html = html.replace(’%%TEMP1%%’,
+Math.round(data.daily.temperature_2m_max[1]));
+
+
+
+html = html.replace(’%%TEMP2%%’,
+Math.round(data.daily.temperature_2m_max[2]));
+
+
+
+html = html.replace(’%%TEMP3%%’,
+Math.round(data.daily.temperature_2m_max[3]));
+
+
+
+html = html.replace(’%%ICON1%%’,
+${icon1}.png);
+
+
+
+html = html.replace(’%%ICON2%%’,
+${icon2}.png);
+
+
+
+html = html.replace(’%%ICON3%%’,
+${icon3}.png);
+
+
+
+fs.writeFileSync(‘jsweatherupdated.html’, html);
 }
 
 updateWeather();
